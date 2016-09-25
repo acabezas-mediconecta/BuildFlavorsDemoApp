c85c98e848877c101b4a0cd5404071f6c05e75df
Alexander Cabezas <alexander.cabezas@fon.com>
2016-09-25 19:41:54 +0200

    second commit

---
+++ app/src/flavor1/java/com/acabezas/buildflavorsdemoapp/test/TestClass.java
@@ -0,0 +1,9 @@
+package com.acabezas.buildflavorsdemoapp.test;
+
+/**
+ * Created by alexander on 25/09/16.
+ */
+public class TestClass {
+
+    public static final String THIS_IS_A_TEST = "Test flavor 1";
+}
--- app/src/main/java/com/acabezas/buildflavorsdemoapp/MainActivity.java
+++ app/src/main/java/com/acabezas/buildflavorsdemoapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.acabezas.buildflavorsdemoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.test_text_view);
+        textView.setText(com.acabezas.buildflavorsdemoapp.test.TestClass.THIS_IS_A_TEST);
+
     }
 }
